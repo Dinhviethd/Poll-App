91f5a7263917ae11f43c350d00c6fc948c86f2d7
Dinhviethd <nguyentadinhviet210805@gmail.com>
2025-07-05 14:36:18 +0700

    ci/cd

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,23 @@
-# FROM node:22.16.0 
+FROM node:22-alpine
 
-# WORKDIR /demoexpress 
-# #tạo thư mục demoexpress và di chuyển vào thư mục đó làm việc
+WORKDIR /demoexpress 
+#tạo thư mục demoexpress nằm trong container và di chuyển vào thư mục đó làm việc
 
-# COPY package*.json . 
-# #sao chép các file package.json và package-lock.json vào thư mục làm việc
+COPY package*.json . 
+#sao chép các file package.json và package-lock.json vào thư mục làm việc
 
-# RUN npm install 
-# #tải các package
+RUN npm install 
+#tải các package
 
-# COPY . . 
-# #sao chép toàn bộ mã nguồn vào thư mục làm việc ở WORKDIR
+COPY . . 
+#sao chép toàn bộ mã nguồn vào thư mục làm việc ở WORKDIR
 
-# EXPOSE 8000 
-# #cho mọi người biết rằng ứng dụng sẽ chạy trên cổng 3000
+EXPOSE 8000 
+#cho mọi người biết rằng ứng dụng sẽ chạy trên cổng 3000
+# Ta hiểu rằng khi chạy container thì chương trình trong demoexpress sẽ chạy trên cổng localhost 8000 của container
+# Cổng localhost 8000 này khác với cổng localhost 8000 trên máy tính của chúng ta, nó là cổng riêng biệt của thằng container
 
-# CMD ["npm", "run", "dev"] 
+CMD ["npm", "run", "dev"] 
 # #chạy lệnh npm dev khi khởi động container
-
-FROM node:18-alpine
-WORKDIR /app
-COPY . .
-RUN npm install --production
-EXPOSE 8000
-CMD ["node", "main.js"]
+#Có thể hiểu container giống như 1 phiên bản nhỏ hơn của máy tính ta đang chứa vậy. Máy tính ta có thể chứa nhiều container
+#Các container có thể mở các port 3000 8000 gì đó để máy tính ta có thể truy cập vào container đó
--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import db from './src/database/poll.database.js'
-import routes from './src/routes/poll.routes.js'
+import router from './src/routes/index.js'
+import mongoInstance from './src/configs/mongoose.config.js'
+const app = express()
 dotenv.config()
 
-const app = express()
-const PORT = process.env.PORT || 3000
-const ConnectionString = process.env.ConnectionString || ""
-try
-{
-    await db.Connect(ConnectionString)
-    app.use(express.json())
-    app.use("/", routes)
-    app.use((req, res) => {
-        res.status(404).json({message: "Không tìm thấy route hợp lệ"})
-    })
-    //////
-    app.use((err, req, res, next) => {
-        console.error(err.stack)
-        res.status(500).json({
-            error: err.message,
-            message: "Đây là middleware error cuối cùng"
-        })
-    })
-    app.listen(PORT, () => {
-        console.log(`Server của mày đang chạy trên http://localhost:${PORT}`)
-    })
+async function startServer() {
+  const connectString = process.env.MONGODB_URI
+  console.log('Connecting to MongoDB...')
+  await mongoInstance.connect(connectString)
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use('/api', router)
+
+  app.use((req, res) => {
+    res.status(404).send('Not Found')
+  })
+  app.use((err, req, res) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server run at http://localhost:${PORT}`)
+  })
 }
-catch(error)
-{
-    console.log("Lỗi khi bật server: ", error)
+try {
+    await startServer()
+} catch (error) {
+    console.error('Error starting server:', error)
 }
